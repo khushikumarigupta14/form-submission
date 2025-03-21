c1d47355bb14be094e0fe1aec27222848cec7082
khushikumarigupta14 <khushikumarigupta463@gmail.com>
2025-03-21 20:57:30 +0530

    add server.js to initialize Express server and connect to MongoDB

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+// server.js
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import connectDB from "./src/config/db.js";
+import config from "./src/config/config.js";
+import formRoutes from "./src/User/FormRoutes.js";
+
+dotenv.config();
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Routes
+app.use("/api/form", formRoutes);
+
+// Connect to MongoDB
+connectDB();
+app.listen(config.PORT, () =>
+  console.log(`Server running on port ${config.PORT}`)
+);
