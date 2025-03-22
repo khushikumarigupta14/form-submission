258ec43e2fdc76903fe0bff45b4ed84f40fee91e
khushikumarigupta14 <khushikumarigupta463@gmail.com>
2025-03-22 06:59:11 +0530

    feat: configure CORS to allow requests from frontend URL

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,12 @@ dotenv.config();
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: config.frontend_url,
+  })
+);
+
 app.use(express.json());
 
 // Routes
--- backend/src/config/config.js
+++ backend/src/config/config.js
@@ -6,6 +6,7 @@ dotenv.config();
 const config = {
   MONGO_URI: process.env.MONGO_URI || "mongodb://localhost:27017/yourDatabase",
   PORT: process.env.PORT || 5000,
+  frontend_url: process.env.FRONTEND_URL || "http://localhost:5173/",
 };
 
 export default config;
