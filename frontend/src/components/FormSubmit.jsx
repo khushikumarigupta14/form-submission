66bfe479273e1020257279319168033d007ab8bc
khushikumarigupta14 <khushikumarigupta463@gmail.com>
2025-03-22 11:23:17 +0530

    feat: add world-countries package and implement country selection in form

--- frontend/src/components/FormSubmit.jsx
+++ frontend/src/components/FormSubmit.jsx
@@ -4,6 +4,13 @@ import { useNavigate, useParams } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import countries from "world-countries"; // Import world-countries
+
+// Convert country data to a mapping { "USA": "United States", "IND": "India", ... }
+const countryOptions = countries.reduce((acc, country) => {
+  acc[country.cca2] = country.name.common;
+  return acc;
+}, {});
 const FormSubmit = () => {
   const { id } = useParams();
   const [formData, setFormData] = useState({
@@ -43,6 +50,7 @@ const FormSubmit = () => {
     setFormData({
       ...formData,
       [name]: type === "checkbox" ? checked : value,
+      ...(name === "country" && { country: countryOptions[value] || "" }),
     });
   };
   const validateForm = () => {
@@ -243,15 +251,20 @@ const FormSubmit = () => {
             </label>
             <select
               name="country"
-              value={formData.country}
+              value={
+                Object.keys(countryOptions).find(
+                  (key) => countryOptions[key] === formData.country
+                ) || ""
+              }
               onChange={handleChange}
               className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500"
             >
               <option value="">Select Country</option>
-              <option value="usa">USA</option>
-              <option value="canada">Canada</option>
-              <option value="uk">UK</option>
-              <option value="australia">Australia</option>
+              {Object.entries(countryOptions).map(([code, name]) => (
+                <option key={code} value={code}>
+                  {name}
+                </option>
+              ))}
             </select>
           </div>
 
